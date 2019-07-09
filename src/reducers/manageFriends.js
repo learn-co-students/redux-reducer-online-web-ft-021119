74e81abba440c7ab31b7304c99f78829f0a14b01
Burak Kocak <burakkocak884@ghotmail.com>
2019-07-09 12:09:48 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+ if (action.type === 'ADD_FRIEND'){
+ 	return {...state, friends: state.friends.concat(action.friend)}
+ } else if (action.type === 'REMOVE_FRIEND'){
+return {...state, friends: state.friends.filter(friend =>friend.id !== action.id)}
+
+ } else {
+ 	return state;
+ }
+
+
 }
+
+
